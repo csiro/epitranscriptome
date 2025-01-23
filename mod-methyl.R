4a64dc2f9c6050094d53867547bc75518d652be5
brad-knox <107171539+brad-knox@users.noreply.github.com>
2025-01-23 12:08:03 +1100

    mod-methyl plots ... still need to modularise the filter menu

--- mod-methyl.R
+++ mod-methyl.R
@@ -17,7 +17,14 @@ methyl_UI <- function(id){
     ),
     mainPanel(
       plotOutput(ns('metacoord')),
-      #DTOutput(ns('table_out'))
+      fluidRow(
+        column(6,
+               plotOutput(ns("gene_density"))
+        ),
+        column(6,
+               plotOutput(ns("gene_swarm"))
+        )
+      )
     )
   )
 }
@@ -71,9 +78,39 @@ methyl_server <- function(id, rvals){
                                      y = metacoord_sig_ratio, 
                                      color = sample_label)) +
           geom_line(alpha = 0.4, size = 0.6) +
-          ggtitle(paste0("Significant Sites vs Metacoordinate"))
+          ggtitle(paste0(id, " Significant Site Ratio vs Metacoordinate"))
         
         fig
+      })
+      
+      output$gene_density <- renderPlot({
+        dt <- dt_subset()
+        if ((nrow(dt) > 0) && (length(rvals$genes) > 0)){
+          fig <- ggplot(dt[gene_id %in% rvals$genes],
+                        aes(x = position, y = rolling_meth_density_normed, color = sample_label)) + 
+            geom_point() +
+            geom_smooth() + 
+            geom_rug(aes(x = position - up_junc_dist, y = NULL, color = NULL), sides = "b") +
+            facet_grid(rows = vars(transcript_id), cols = vars(gene_id)) + 
+            ggtitle(paste0(id, " Rolling Average Methylation Density"))
+          
+          fig
+        }
+      })
+      
+      output$gene_swarm <- renderPlot({
+        dt <- dt_subset()
+        if ((nrow(dt) > 0) && (length(rvals$genes) > 0)){
+          fig <- ggplot(dt[gene_id %in% rvals$genes],
+                        aes(x = position, y = sample_label, color = methylated)) +
+            geom_beeswarm(size = 1, cex = 1, priority = "density") +
+            guides(color = 'none') +
+            geom_rug(aes(x = position - up_junc_dist, y = NULL, color = NULL), sides = "b") +
+            facet_grid(rows = vars(transcript_id), cols = vars(gene_id)) +
+            ggtitle(paste0(id, " Methylation Sites"))
+          
+          fig
+        }
       })
       
       output$table_out <- renderDT({
@@ -88,37 +125,67 @@ methyl_server <- function(id, rvals){
         rvals$methyl_rds <- input$methyl_file$datapath
       })
       
+      observeEvent(input$transcript_type, {
+        print("transcript type selected:")
+        rvals$transcript_types <- if (is.null(input$transcript_type)){ list() } else { input$transcript_type }
+        print(rvals$transcript_types)
+        
+        if (nrow(rvals$methyl) > 0){
+          if (length(rvals$transcript_types) > 0){
+            gene_list <- unique(rvals$methyl[transcript_type %in% rvals$transcript_types]$gene_id)
+          }  else {
+            gene_list <- unique(rvals$methyl$gene_id)
+          }
+          updateSelectizeInput(session, "genes", choices=gene_list, selected=NULL, server = TRUE)
+        }
+      }, ignoreNULL = FALSE)
+      
+      # for the genes selectizeInput, we want to be able to detect when all 
+      # entries have been cleared == NULL
+      observeEvent(input$genes, {
+        print("genes selected:")
+        rvals$genes <- if (is.null(input$genes)){ list() } else { input$genes }
+        if ((length(rvals$genes) < 24) && (length(rvals$genes) > 0)){
+          transcript_list <- unique(rvals$methyl[gene_id %in% rvals$genes, transcript_id])
+          updateSelectizeInput(session, "transcripts", choices=transcript_list, selected=NULL, server = TRUE)
+        } else {
+          updateSelectizeInput(session, "transcripts", choices=NULL, selected=NULL, server = TRUE)
+        }
+        print(rvals$genes)
+      }, ignoreNULL = FALSE)
+      
+      observeEvent(input$transcripts, {
+        print("transcripts selected:")
+        rvals$transcripts <- if (is.null(input$transcripts)){ list() } else { input$transcripts }
+        print(rvals$transcripts)
+      }, ignoreNULL = FALSE)
+      
+      # user input for the methyl file name
+      observeEvent(input$methyl_file, {
+        print("methyl file touched")
+        print(input$methyl_file)
+        rvals$methyl_rds <- input$methyl_file$datapath
+      })
+      
       # new file to upload ... load it
-      # note that we have this as seperate from observe(input$polya_file)
+      # note that we have this as seperate from observe(input$methyl_file)
       # to detect the initial value
       observe({
         rvals$methyl_rds
         print("doing the methyl load...")
         if (file.exists(rvals$methyl_rds)){
-          rvals$methyl <- readRDS(rvals$methyl_rds)
-          
-          print("doing metacoord ratio")
-          # metacoordinate binning ... like R2_plotMetaTranscript
-          # subdivide the [0,3] interval
-          metacoord_breaks <- seq(0, 3, 0.025)
-          
-          # out_ratio <- methyl[, interval := cut(transcript_metacoordinate, metacoord_breaks, include.lowest = TRUE, right = TRUE, labels = FALSE)][, .(n =  .N), by = .(interval, meth_type, sample_label)][, dcast(.SD, interval ~ filter, value.var = "n", fill = 0)][, ratio := sig / (sig + ns)]
-          
-          # add a new categorical interval label based on metacoordinate
-          out_ratio <- rvals$methyl[, metacoord_interval := cut(transcript_metacoordinate, metacoord_breaks, include.lowest = TRUE, right = TRUE, labels = FALSE)]
-          
-          # aggregate by metacoordinate, meth_type, sample_label (ie. over all contigs)
-          out_ratio <- out_ratio[, .(n = .N, sig = sum(methylated)), by = .(metacoord_interval, meth_type, sample_label)]
-          
-          out_ratio <- na.omit(out_ratio)
-          
-          #out_ratio <- out_ratio[, metacoord_sig_sites := sum(methylated), by = .(metacoord_interval, meth_type, sample_label)]
-          
-          out_ratio <- out_ratio[, metacoord_sig_ratio := sig / n, by = .(metacoord_interval, meth_type, sample_label)]
-          
-          rvals$out_ratio <- out_ratio
+          rvals$methyl <- readRDS(rvals$methyl_rds) 
         }
         print("...loaded")
+      })
+      
+      # whole new file, reset everything
+      observe({
+        rvals$methyl
+        ttypes <- unique(rvals$methyl$transcript_type)
+        gene_list <- unique(rvals$methyl$gene_id)
+        updateSelectizeInput(session, "transcript_type", choices=ttypes, selected=NULL, server = TRUE)
+        updateSelectizeInput(session, "genes", choices=gene_list, selected=NULL, server = TRUE)
       })
     }
   )
